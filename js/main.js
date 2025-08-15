582f608736f34be978dd3da199558e580e90a1fd
Jasurbek465 <abdurashidovjasurbek584@gmail.com>
2025-08-15 13:26:21 +0500

    medai is complited

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const offersSwiper = new Swiper('.special-offers__swiper', {
+  // loop: true,
+  spaceBetween: 20,
+  pagination: {
+    el: '.special-offers__pagination',
+    clickable: true,
+  },
+});
