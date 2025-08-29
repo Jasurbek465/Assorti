073dc89e8947de739506b716a021f22ebc138594
Jasurbek465 <abdurashidovjasurbek584@gmail.com>
2025-08-29 14:33:01 +0500

    m

--- js/main.js
+++ js/main.js
@@ -440,3 +440,67 @@ var swiper9 = new Swiper(".collapsed-slide", {
     },
   },
 });
+
+
+var swiper = new Swiper(".threeSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 40,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    // mobile
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // laptop va kattaroq
+    1103: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    }
+  }
+});
+var swiper = new Swiper(".mySwiper", {});
+var swiper = new Swiper(".introSwiper ", {
+  pagination: {
+    el: ".swiper-pagination-grey",
+    clickable: true,
+  },
+});
+var swiper = new Swiper(".productSwiper", {
+  slidesPerView: 2.3,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: ".product-swiper-next",
+    prevEl: ".product-swiper-prev",
+  },
+});
+
+var swiper = new Swiper(".usefulSwiper", {
+  slidesPerView: 7,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: ".product-swiper-next-bottom",
+    prevEl: ".product-swiper-prev-bottom",
+  },
+});
+var swiper = new Swiper(".feedbackSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: ".product-swiper-next-2",
+    prevEl: ".product-swiper-prev-2",
+  },
+});
+
+
